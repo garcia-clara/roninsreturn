4ed75def11aa9d07c40bda01535da7c41f192e2c
Clara <92577997+garcia-clara@users.noreply.github.com>
2024-01-07 16:32:32 +0100

    healthbar

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int maxHealth = 100;
+    public int currentHealth;
+
+    public HealthBar healthBar;
+    void Start()
+    {
+        currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
+    }
+
+    void Update(){
+        if (Input.GetKeyDown(KeyCode.H)){
+            TakeDamage(20);
+        }
+    }
+
+    void TakeDamage(int damage){
+        currentHealth = currentHealth - damage;
+        healthBar.SetHealth(currentHealth);
+    }
+
+}
